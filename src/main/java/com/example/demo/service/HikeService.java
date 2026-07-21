5887e2872cfc5ee89d431a002cb3ac4a56e2007a
Gavrila Alexandru <alexgavrila2004@gmail.com>
2026-07-21 12:45:34 +0300

    Am adaugat Transaction management

--- src/main/java/com/example/demo/service/HikeService.java
+++ src/main/java/com/example/demo/service/HikeService.java
@@ -5,9 +5,11 @@ import com.example.demo.model.Hike;
 import com.example.demo.repository.HikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class HikeService {
 
     private final HikeRepository hikeRepository;
